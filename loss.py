7d077eaa0ea12f32891f1069bb7e00f01cfe4e0c
Yuyang Zhao <43061147+HeliosZhao@users.noreply.github.com>
2020-11-26 10:49:40 +0800

    add small loss

--- loss.py
+++ loss.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn.functional as F
 import math
+import copy
 
 class NoisyLabelLoss(torch.nn.Module):
     def __init__(self, args, num_classes=10):
@@ -26,26 +27,35 @@ class NoisyLabelLoss(torch.nn.Module):
         loss = ce * self.alpha + rce * self.beta
         return loss
 
-# class NoisyLabelLoss(torch.nn.Module):
-#     def __init__(self, args, num_classes=10):
-#         super(NoisyLabelLoss, self).__init__()
-#         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-#         self.alpha = args.alpha
-#         self.beta = args.beta
-#         self.num_classes = num_classes
-#         self.cross_entropy = torch.nn.CrossEntropyLoss()
-#
-#     def forward(self, pred, labels):
-#         # CCE
-#         ce = self.cross_entropy(pred, labels)
-#
-#         # RCE
-#         pred = F.softmax(pred, dim=1)
-#         pred = torch.clamp(pred, min=1e-7, max=1.0)
-#         label_one_hot = torch.nn.functional.one_hot(labels, self.num_classes).float().to(self.device)
-#         label_one_hot = torch.clamp(label_one_hot, min=1e-4, max=1.0)
-#         rce = (-1*torch.sum(pred * torch.log(label_one_hot), dim=1))
-#
-#         # Loss
-#         loss = self.alpha * ce + self.beta * rce.mean()
-#         return loss
+class SmallLoss(torch.nn.Module):
+    def __init__(self, args, num_classes=10):
+        super(SmallLoss, self).__init__()
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.args = args
+        self.alpha = args.alpha
+        self.beta = args.beta
+        self.num_classes = num_classes
+        self.cross_entropy = torch.nn.CrossEntropyLoss()
+        self.A = args.A
+
+    def forward(self, pred, labels, clean_rate=1):
+        # ce = self.cross_entropy(pred, labels)
+
+        pred = F.softmax(pred, dim=1)
+        pred = torch.clamp(pred, min=1e-7, max=1.0)
+        label_one_hot = F.one_hot(labels, self.num_classes).float().to(self.device)
+        label_ce = copy.deepcopy(label_one_hot)
+        min_clamp = math.exp(self.A)
+        label_one_hot = torch.clamp(label_one_hot, min=min_clamp, max=1.0)
+        rce = (-1 * torch.sum(pred * torch.log(label_one_hot), dim=1))
+        ce  = (-1 * torch.sum(label_ce * torch.log(pred), dim=1))
+        # print('ce size = ',ce.size())
+        rank = F.softmax(rce, dim=0) * self.beta + F.softmax(ce, dim=0) * self.alpha
+        _, indices = rank.sort()
+
+        # num_clean = int(pred.size(0) * (1-self.args.eta))
+        num_clean = int(pred.size(0) * clean_rate)
+        indices = indices[:num_clean]
+        # print(ce[indices].shape)
+        loss = ce[indices].mean() * self.alpha + rce[indices].mean() * self.beta
+        return loss
--- trainer.py
+++ trainer.py
@@ -79,6 +79,48 @@ class Trainer(object):
     def _forward(self, inputs):
         return self.model(inputs)
 
+class SmallLossTrainer(Trainer):
+    def train(self, train_loader, optimizer, epoch, train_iters=0):
+        self.model.train()
+        batch_time = AverageMeter()
+        losses = AverageMeter()
+        acc_meters = AverageMeter()
+        acc5_meters = AverageMeter()
+        clean_rate = self.clean_rate(epoch)
+        print('clean_rate at epoch {:d} is {:.2f}'.format(epoch, clean_rate))
+
+        with torch.autograd.set_detect_anomaly(True):
+            for i, batch_data in enumerate(train_loader):
+                imgs, labels = self._parse_data(batch_data)
+                start_time = time.time()
+                pred = self._forward(imgs)
+                loss = self.criterion(pred, labels, clean_rate)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                acc, acc5 = accuracy(pred, labels, topk=(1, 5))
+
+
+                losses.update(loss.item())
+                acc_meters.update(acc.item())
+                acc5_meters.update(acc5.item())
+
+                batch_time.update(time.time() - start_time)
+
+                if (i + 1) % self.args.print_freq == 0:
+                    print('Epoch: [{}][{}/{}]\t'
+                          'Time {:.3f} ({:.3f})\t'
+                          'Loss {:.3f}({:.3f})\t'
+                          'Accuracy {:.3f}/{:.3f}'
+                          .format(epoch, i + 1, len(train_loader),
+                                  batch_time.val, batch_time.avg,
+                                  losses.val, losses.avg,
+                                  acc_meters.val, acc5_meters.val))
+    def clean_rate(self, epoch):
+        noise_weight = min(1, epoch/self.args.T)
+        return (1 - self.args.eta * noise_weight)
 
 class CleanNetTrainer(Trainer):
     def train(self, train_loader, optimizer, epoch, train_iters=0):
